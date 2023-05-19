d9a68248e2f1dbe07d8f5b575549330bcb0eabe2
pancake363 <105073056+pancake363@users.noreply.github.com>
2023-05-19 16:07:09 +0300

    added similar products

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
