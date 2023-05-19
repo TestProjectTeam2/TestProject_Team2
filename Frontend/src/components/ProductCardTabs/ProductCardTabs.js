ec8b84b381eb47f5653ad6167bf1d05f86e7955d
pancake363 <105073056+pancake363@users.noreply.github.com>
2023-05-19 18:37:02 +0300

    added charecters and rates in tab

--- Frontend/src/components/ProductCardTabs/ProductCardTabs.js
+++ Frontend/src/components/ProductCardTabs/ProductCardTabs.js
@@ -5,7 +5,8 @@ const about = [
 	{
 		id: 1,
 		title: 'Про товар',
-		description: `Новітня відеокарта NVIDIA GeForce RTX серії 30 і продуктивний процесор Intel Core 11-го покоління, 
+		description: false,
+		shortDescreption: `Новітня відеокарта NVIDIA GeForce RTX серії 30 і продуктивний процесор Intel Core 11-го покоління, 
 		встановлений у максимальній конфігурації цього ноутбука, подбають, щоб твій ігровий потенціал сягнув нового рівня. 
 		Виходь на один рівень із геймерами-професіоналами і завдяки Ideapad 3i Gaming. Керуй зручно та точно з клавіатурою, 
 		що підтримує 100% N-Key Rollover. Поринай у світ надзвичайно плавної графіки з дисплеєм Full HD із матрицею IPS та 
@@ -15,17 +16,72 @@ const about = [
 	}
 ]
 
+const chara = [
+	{
+		id: 1,
+		title: 'Екран',
+		details: [
+			{
+				id:1,
+				name: 'Серія',
+				report: 'IdeaPad Gaming 3'
+			},
+			{
+				id:2,
+				name: 'Діагональ екрана',
+				report: '15.6" (1920x1080) Full HD'
+			},
+			{
+				id:3,
+				name: 'Тип екрану',
+				report: 'IPS'
+			},
+			{
+				id:4,
+				name: 'Роздільна здатність',
+				report: '1920x1080'
+			},
+		]
+	},
+	{
+		id: 2,
+		title: 'Відео-картка',
+		details: [
+			{
+				id:1,
+				name: 'Виробник відеокарти',
+				report: 'Nvidia'
+			},
+			{
+				id:2,
+				name: 'Дискретна відеокарта',
+				report: 'GeForce GTX 1650'
+			},
+			{
+				id:3,
+				name: 'Тип відеокарти',
+				report: 'Дискретна + Інтегрована'
+			},
+			{
+				id:4,
+				name: `Обсяг пам'яті відеокарти`,
+				report: '4 ГБ'
+			},
+			{
+				id:5,
+				name: `Інтегрована відеокарта`,
+				report: 'Iris Xe Graphics'
+			},
+		]
+	},
+]
+
 const characteristics = [
 	{
 		id: 1,
 		title: 'Характеристики',
-		description: `Новітня відеокарта NVIDIA GeForce RTX серії 30 і продуктивний процесор Intel Core 11-го покоління, 
-		встановлений у максимальній конфігурації цього ноутбука, подбають, щоб твій ігровий потенціал сягнув нового рівня. 
-		Виходь на один рівень із геймерами-професіоналами і завдяки Ideapad 3i Gaming. Керуй зручно та точно з клавіатурою, 
-		що підтримує 100% N-Key Rollover. Поринай у світ надзвичайно плавної графіки з дисплеєм Full HD із матрицею IPS та 
-		частотою оновлення кадрів до 165 Гц. Миттєво визначай засідку ворогів з динаміками Nahimic, що відтворюють тривимірне звучання. 
-		Високоефективна система терморегуляції підтримує оптимальну температуру ноутбука, у яких би запеклих віртуальних битвах тобі не 
-		довелося брати участь.`
+		description: chara,
+		shortDescreption: false
 	}
 ]
 
@@ -33,13 +89,34 @@ const rates = [
 	{
 		id: 1,
 		title: 'Оцінки та відгуки',
-		description: `Новітня відеокарта NVIDIA GeForce RTX серії 30 і продуктивний процесор Intel Core 11-го покоління, 
-		встановлений у максимальній конфігурації цього ноутбука, подбають, щоб твій ігровий потенціал сягнув нового рівня. 
-		Виходь на один рівень із геймерами-професіоналами і завдяки Ideapad 3i Gaming. Керуй зручно та точно з клавіатурою, 
-		що підтримує 100% N-Key Rollover. Поринай у світ надзвичайно плавної графіки з дисплеєм Full HD із матрицею IPS та 
-		частотою оновлення кадрів до 165 Гц. Миттєво визначай засідку ворогів з динаміками Nahimic, що відтворюють тривимірне звучання. 
-		Високоефективна система терморегуляції підтримує оптимальну температуру ноутбука, у яких би запеклих віртуальних битвах тобі не 
-		довелося брати участь.`
+		description: false,
+		shortDescreption: false,
+		revies: [
+			{
+				id: 1,
+				userName: 'Варвара',
+				date: '20.03.2023',
+				approve: 'Покупка підтверджена',
+				message: `Lorem ipsum dolor sit amet consectetur. At mauris sed morbi eleifend laoreet at ac egestas arcu. Morbi leo 
+				nec vel egestas arcu est. Fringilla integer ac pulvinar quis turpis. Ultrices in amet volutpat ornare bibendum ac bibendum. 
+				Nulla aliquam quisque faucibus sem pellentesque natoque. A magnis vitae adipiscing aliquam iaculis. Tellus cum velit placerat 
+				molestie urna fringilla. Sit enim sit a eget amet.`,
+				like: '0',
+				dislike: '0',
+			},
+			{
+				id: 2,
+				userName: 'Олександра',
+				date: '12.03.2023',
+				approve: 'Покупка підтверджена',
+				message: `Lorem ipsum dolor sit amet consectetur. At mauris sed morbi eleifend laoreet at ac egestas arcu. Morbi leo 
+				nec vel egestas arcu est. Fringilla integer ac pulvinar quis turpis. Ultrices in amet volutpat ornare bibendum ac bibendum. 
+				Nulla aliquam quisque faucibus sem pellentesque natoque. A magnis vitae adipiscing aliquam iaculis. Tellus cum velit placerat 
+				molestie urna fringilla. Sit enim sit a eget amet.`,
+				like: '0',
+				dislike: '0',
+			},
+		]
 	}
 ]
 
@@ -70,7 +147,7 @@ const tabs = [
 	{
 		id: 4,
 		title: 'Питання та відповіді',
-		content: questines
+		content: characteristics
 	}
 ]
 
@@ -101,10 +178,45 @@ export const ProductCardTabs = () => {
 				<div className='content-main'>
 					{tabs.map(({id, content}) => (
 						<div key={id} className='content-main__container'>
-							{currentTab === `${id}` && <div className='content-main__body'>{content.map(({id, title, description}) => (
+							{currentTab === `${id}` && <div className='content-main__body'>{content.map(({id, title, description, shortDescreption, revies}) => (
 								<div className='content-inside' key={id}>
 									<div className='content-inside__title'>{title}</div>
-									<div className='content-inside__description'>{description}</div>
+									<div className='content-inside__description'>{description ? description.map( ({id, title, details}) => (
+										<div className='description' key={id}>
+											<div className='description__subtitle'>{title}</div>
+											<div className='description__details'>{details.map(({id, name, report}) => (
+												<div className='report d-flex justify-content-between align-items-center' key={id}>
+													<div className='report__name'>{name}</div>
+													<div className='report__underline mx-2'>_</div>
+													<div className='report__info'>{report}</div>
+												</div>))}
+											</div>
+										</div>
+									)) 
+								: shortDescreption ? shortDescreption : revies.map(({id, userName, date, approve, message, like, dislike}) => (
+									<div className='revies' key={id}>
+										<div className='revies__body mb-2'>
+										<div className='revies__top d-flex justify-content-between align-items-center mb-3'>
+											<div className='revies__title'>
+												<div className='revies__user-name mb-1'>{userName}</div>
+												<div className='revies__confirmation'><span></span>{approve}</div>
+											</div>
+											<div className='revies__date align-self-start'>{date}</div>
+										</div>
+										<div className='revies__message'>{message}</div>
+										</div>
+										<div className='revies__likes d-flex justify-content-end align-items-end text-right'>
+											<div className='revies__dislike d-flex'>
+												<img src='/images/icons/thumb_down.svg'/>
+												<div className='mx-1 align-self-end'>{dislike}</div>
+											</div>
+											<div className='revies__like d-flex'>
+												<img src='/images/icons/thumb_up.svg'/>
+												<div className='mx-1 align-self-end'>{like}</div>
+											</div>
+										</div>
+									</div>
+								))}</div>
 							</div>
 							))}</div>}
 						</div>
