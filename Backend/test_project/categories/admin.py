d8572a2feeed797bfbc2d2ab669e6788d66df366
antony-kosenko <kosenko.anton.work@gmail.com>
2023-05-20 00:25:39 +0300

    naming corrections

--- Backend/test_project/categories/admin.py
+++ Backend/test_project/categories/admin.py
@@ -4,8 +4,8 @@ from django_mptt_admin.admin import DjangoMpttAdmin
 from .models import Category
 
 
-class CountryAdmin(DjangoMpttAdmin):
+class CategoryAdmin(DjangoMpttAdmin):
     pass
 
 
-admin.site.register(Category, CountryAdmin)
+admin.site.register(Category, CategoryAdmin)
