ed0fc23dfd0025092fea144494ee2b58c20ab50d
lolesia <alesyavaskovith@gmail.com>
2023-05-20 13:31:57 +0300

    full model Order and serializer

--- Backend/test_project/order/serializers.py
+++ Backend/test_project/order/serializers.py
@@ -3,8 +3,12 @@ from .models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField()
+    user = serializers.SlugRelatedField(slug_field='user', queryset=User.objects.all())
+    name = serializers.SlugRelatedField(many=True, slug_field='name', queryset=Product.objects.all())
+    id = serializers.SlugRelatedField(many=True, slug_field='id', queryset=Product.objects.all())
+    discount = serializers.SlugRelatedField(many=True, slug_field='discount', queryset=Product.objects.all())
+    price = serializers.SlugRelatedField(many=True, slug_field='price', queryset=Product.objects.all())
 
     class Meta:
         model = Order
-        field = '__all__'
+        fields = '__all__'
