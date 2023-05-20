d929333bd17a23540bc02a4c27c74bec00e4cd25
lolesia <alesyavaskovith@gmail.com>
2023-05-20 13:31:57 +0300

    full model Order and serializer

--- Backend/test_project/order/models.py
+++ Backend/test_project/order/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import uuid
+from users.models import User
+from product.models import Product
 
 
 class Order(models.Model):
@@ -16,9 +18,13 @@ class Order(models.Model):
         ('NC', 'Non-cash'),
         ('IN', 'Installments')
     ]
-    order_id = models.UUIDField(primary_key = True, default=uuid.uuid4, editable=False)
-    # name = models.ManyToManyField()
-    # user = models.ForeignKey(User, on_delete=models.SET('видалений користувач'), null=True ,verbose_name='Користувач')
+    order_id = models.UUIDField(primary_key= True, default=uuid.uuid4, editable=False)
+    name = models.ManyToManyField(Product, verbose_name='Назва товару')
+    id = models.ManyToManyField(Product, verbose_name='Номер товару')
+    price = models.ManyToManyField(Product, verbose_name='Ціна')
+    quantity = models.IntegerField(default=1, verbose_name='Кількість')
+    discount = models.ManyToManyField(Product, verbose_name='Знижка')
+    user = models.ForeignKey(User, on_delete=models.SET('видалений користувач'), null=True ,verbose_name='Користувач')
     first_name = models.CharField(max_length=30, verbose_name="Ім'я")
     last_name = models.CharField(max_length=50, verbose_name='Прізвище')
     phone = models.CharField(max_length=30, verbose_name='Номер телефону')
@@ -26,4 +32,12 @@ class Order(models.Model):
     сity = models.CharField(max_length=30, verbose_name='Ваше місто')
     delivery_way = models.CharField(max_length=10, choises=DELIVERY, verbose_name='Спосіб доставки')
     payment = models.CharField(max_length=10,choices=PAYMENT)
+
+    @property
+    def total_price(self):
+        total = 0
+        for product in self.name.all():
+            total += product.price * self.quantity
+        return total
+
 
