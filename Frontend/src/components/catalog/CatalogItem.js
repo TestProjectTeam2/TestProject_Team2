64a280e295d9dc3e2cb87e0146f85cfc839a996a
Nadiia <malicenkonadic@gmail.com>
2023-05-22 14:08:29 +0300

    Improve catalog in header

--- Frontend/src/components/catalog/CatalogItem.js
+++ Frontend/src/components/catalog/CatalogItem.js
@@ -1,37 +1,22 @@
-import React, { useState, useRef } from 'react';
-import { Overlay} from 'react-bootstrap';
+import React from 'react';
 
-export default function CatalogItem({ title, subcategories }) {
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
-
+export default function CatalogItem({ subcategories }) {
   return (
     <>
-      <button ref={target} onMouseEnter={() => setShow(!show)} className='categories'>
-        <a>{title}</a>
-        <img src="/images/icons/next.svg" />
-      </button>
-      <Overlay target={target.current} show={show} placement="right">
-        {({ show: _show, ...props }) => (
-          <div
-            className="sub-categories"
-            {...props}
-          >
-              {subcategories.map((subcategoryObj) => {
-              const subcategoryName = Object.keys(subcategoryObj)[0];
-              const subcategoryValues = subcategoryObj[subcategoryName];
-              return (
-                <div key={subcategoryName} className='sub-category'>
-                  <h4>{subcategoryName}</h4>
-                  {subcategoryValues.map((value) => (
-                    <p key={value}>{value}</p>
-                  ))}
-                </div>
-              );
-            })}
-          </div>
-        )}
-      </Overlay>
+      <div className='sub-categories'>
+          {subcategories.map((subcategoryObj) => {
+          const subcategoryName = Object.keys(subcategoryObj)[0];
+          const subcategoryValues = subcategoryObj[subcategoryName];
+          return (
+            <div key={subcategoryName} className='sub-category'>
+              <h4>{subcategoryName}</h4>
+              {subcategoryValues.map((value) => (
+                <p key={value}>{value}</p>
+              ))}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
