442356d99619204929a025511f0399aa585e577a
lolesia <alesyavaskovith@gmail.com>
2023-05-22 17:48:34 +0300

    installed in settings/ urls.py include

--- Backend/test_project/test_project/settings.py
+++ Backend/test_project/test_project/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # test_project apps
     'users',
+    'order.apps.OrderConfig',
     "categories.apps.CategoriesConfig",
     'product',
     # third-party apps
