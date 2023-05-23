723f87a164a67f0f029e22ec826ada028a85c4cd
pancake363 <105073056+pancake363@users.noreply.github.com>
2023-05-23 10:21:01 +0300

    added react-router-dom and redux provider in index

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,13 +5,20 @@ import './styles/index.scss';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider as ReduxProvider } from "react-redux";
+import { rootReducer } from './store/rootReducer';
+
+const store = configureStore({ reducer: rootReducer });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+	<ReduxProvider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</ReduxProvider>
   </React.StrictMode>
 );
 
