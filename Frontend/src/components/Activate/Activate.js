02f1d675959b7165a837afa004d827e062aa4e88
pancake363 <105073056+pancake363@users.noreply.github.com>
2023-05-23 15:15:12 +0300

    added activate component and fix actions & reducer

---
+++ Frontend/src/components/Activate/Activate.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { useMutation } from "../../hooks/useMutation";
+import { authenticateUser } from '../../store/actions/auth';
+
+export const Activate = (match) => {
+
+	const [verified, setVerified] = useState(false);
+
+	const activationUrl = 'http://127.0.0.1:8000/auth/users/activation/'
+
+	const dispatch = useDispatch();
+
+	const {mutate} = useMutation({
+		url: activationUrl,
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		onSuccess: () => {
+			// Handle errors
+			alert('Запит пройшов успішно');
+		},
+		// onError: () => alert('Запит не був відправлений')
+	});
+
+	const { uid } = useParams();
+	const { token } = useParams();
+
+	const verifyAccount = e => {
+		mutate(JSON.stringify({ uid, token }));
+      setVerified(true);
+	}
+
+	if (verified) {
+		return <Navigate to="/login"/>;
+	}
+
+	return (
+	<div className="activate d-flex justify-content-center align-items-center my-5">
+		<div className="activate-container text-center p-5">
+			<h1 className="my-5">Activate your account</h1>
+			<Button type="button"  className="my-5" onClick={verifyAccount}>Activate</Button>
+		</div>
+	</div>
+	)
+}
+
+// export default Activate;
