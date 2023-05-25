680698045bdca8f57b5eae730ba9aeeb2f783d49
Nadiia <malicenkonadic@gmail.com>
2023-05-25 19:26:56 +0300

    Create order component

--- Frontend/src/containers/App.js
+++ Frontend/src/containers/App.js
@@ -6,6 +6,8 @@ import Error from '../pages/Error/Error';
 import { AuthWindow } from '../components/AuthWindow/AuthWindow';
 import {ProductCardPage} from '../pages/ProductCardPage/ProductCardPage';
 import { Activate } from '../components/Activate/Activate';
+import OrderPage from '../pages/OrderPage/OrderPage';
+import CartPage from '../pages/CartPage/CartPage';
 
 function App() {
   return (
@@ -15,14 +17,15 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path='/login' element={<AuthWindow />} />
           <Route path='/product' element={<ProductCardPage />} />
-          {/* <Route path='/cart' element={<Cart />} /> */}
+          <Route path='/cart' element={<CartPage />} />
           {/* <Route path='/cabinet' element={<Cabinet />}>
               <Route path='/personal-info' element={<PersonalInfo />} />
               <Route path='/favorite' element={<Favorite />} />
           </Route> */}
           {/* <Route path='/product-list' element={<ProductList />} /> */}
-			<Route path='/login/activate/:uid/:token' element={<Activate/>} />
+			    <Route path='/login/activate/:uid/:token' element={<Activate/>} />
         </Route>
+        <Route path='/order' element={<OrderPage />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </div>
---
+++ Frontend/src/pages/OrderPage/OrderPage.js
@@ -0,0 +1,198 @@
+import React from "react";
+import { Container, Row, Col, Tab, Nav, Form, FormGroup, FormLabel, FormCheck } from "react-bootstrap";
+import CartItem from "../CartPage/CartItem";
+import {AuthWindow} from '../../components/AuthWindow/AuthWindow';
+import './OrderPage.scss';
+import { NavLink } from "react-router-dom";
+
+export default function OrderPage() {
+
+    return(
+        <div className="order-page">
+            <header>
+                <img alt="logo" src="/images/icons/TechZoneDark.svg"/>
+                <hr />
+            </header>
+            <Container>
+                <Row>
+                    <Col md={8}>
+                        <h2>Оформлення замовлення</h2>
+                        <h3>Ваше замовлення</h3>
+                        <NavLink to="/cart">
+                            <button className="edit"><img src="/images/icons/edit.svg" alt="edit changes" />Редагувати</button>
+                        </NavLink>
+                        <CartItem mutability={false}/>
+                        <div className="contact-info">
+                            <h5>1. Ваші контактні дані</h5>
+                            <div className="warning">
+                                <p>Увага! Отримання замовлення за паспортом. Введіть прізвище, ім'я, по батькові як зазначено<br /> у документі та мобільний номер телефону отримувача замовлення</p>
+                            </div>
+                            <Tab.Container defaultActiveKey="first">
+                                <Row>
+                                    <Col>
+                                    <Nav variant="pills" className="flex">
+                                        <Nav.Item>
+                                        <Nav.Link eventKey="first">Я новий покупець</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                        <Nav.Link eventKey="second">Я постійний покупець</Nav.Link>
+                                        </Nav.Item>
+                                    </Nav>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                    <Tab.Content>
+                                        <Tab.Pane eventKey="first">
+                                            <form className="new-client">   
+                                                <div>
+                                                    <label htmlFor="lastName" >Прізвище</label>
+                                                    <input id="lastName" type="text" placeholder="Прізвище"/>
+                                                </div> 
+                                                <div>
+                                                    <label htmlFor="firstName">Ім'я</label>
+                                                    <input id="firstName" type="text" placeholder="Ім'я"/>
+                                                </div>
+                                                <div>
+                                                    <label htmlFor="phoneNumber">Номер телефону</label>
+                                                    <input id="phoneNumber" type="tel" placeholder="+38-0XX-XXX-XX-XX" />
+                                                </div>
+                                                <div>
+                                                    <label htmlFor="email">E-mail</label>
+                                                    <input id="email" type="email" placeholder="example@go.com" />
+                                                </div>
+                                            </form>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="second">
+                                            <AuthWindow />
+                                        </Tab.Pane>
+                                    </Tab.Content>
+                                    </Col>
+                                </Row>
+                            </Tab.Container>
+                        </div>
+                        <div className="delivery">
+                    <h5>2. Доставка</h5>
+                    <Form>
+                        <FormGroup>
+                        <FormCheck
+                            type="radio"
+                            name="delivery"
+                            className="custom-radio"
+                            id="deliveryChoice1"
+                            value="Самовивіз із наших магазинів"
+                            label="Самовивіз із наших магазинів"
+                            checked
+                        />
+                        <FormCheck
+                            type="radio"
+                            name="delivery"
+                            className="custom-radio"
+                            id="deliveryChoice2"
+                            value="Кур’єр на вашу адресу"
+                            label="Кур’єр на вашу адресу"
+                        />
+                        <FormCheck
+                            type="radio"
+                            name="delivery"
+                            className="custom-radio"
+                            id="deliveryChoice3"
+                            value="Самовивіз із мобільних точок"
+                            label="Самовивіз із мобільних точок"
+                        />
+                        <FormCheck
+                            type="radio"
+                            name="delivery"
+                            className="custom-radio"
+                            id="deliveryChoice4"
+                            value="Доставка на пошту"
+                            label="Доставка на пошту"
+                        />
+                        </FormGroup>
+                    </Form>
+                    </div>
+                    <div className="pay">
+                    <h5>3. Оплата</h5>
+                    <Form>
+                        <FormGroup>
+                        <FormCheck
+                            type="radio"
+                            name="paymentMethod"
+                            className="custom-radio"
+                            id="paymentChoice1"
+                            value="Оплата під час отримання товару"
+                            label="Оплата під час отримання товару"
+                            checked
+                        />
+                        <FormCheck
+                            type="radio"
+                            name="paymentMethod"
+                            className="custom-radio"
+                            id="paymentChoice2"
+                            value="-5% знижки при оплаті від 500 грн карткою Mastercard Універсальна/Універсальна Gold від ПриватБанк"
+                            label="-5% знижки при оплаті від 500 грн карткою Mastercard Універсальна/Універсальна Gold від ПриватБанк"
+                        />
+                        <FormCheck
+                            type="radio"
+                            name="paymentMethod"
+                            className="custom-radio"
+                            id="paymentChoice3"
+                            value="Оплатити онлайн соціальною картою Пакунок малюка"
+                            label='Оплатити онлайн соціальною картою "Пакунок малюка"'
+                        />
+                        <FormCheck
+                            type="radio"
+                            name="paymentMethod"
+                            className="custom-radio"
+                            id="paymentChoice4"
+                            value="Безготівковий для фізичних осіб"
+                            label="Безготівковий для фізичних осіб"
+                        />
+                        <FormCheck
+                            type="radio"
+                            name="paymentMethod"
+                            className="custom-radio"
+                            id="paymentChoice5"
+                            value="Кредит та оплата частинами"
+                            label="Кредит та оплата частинами"
+                        />
+                        </FormGroup>
+                    </Form>
+                    </div>
+                    </Col>
+                    <Col md={3}>
+                        <form className="promocode">
+                            <label>Промокод</label>
+                            <input />
+                        </form>
+                        <div className="total">
+                            <h2>Разом</h2>
+                            <div className="sum-count">
+                                <p>1 товар на суму</p>
+                                <p>35999 </p>
+                            </div>
+                            <div className="delivery-price">
+                                <p>Доставка</p>
+                                <p>Безкоштовно</p>
+                            </div>
+                            <hr />
+                            <div className="total-price">
+                                <p>До сплати</p>
+                                <p>35999</p>
+                            </div>
+                            <button id="confirm">Підтвердити</button>
+                            <p id="condition">Підтверджуючи замовлення, я приймаю умови:
+                                <li>положення про обробку і захист персональних даних</li>
+                                <li>угоди користувача</li>
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <footer>
+                <hr />
+                <p>2023 © всі права захищені</p>
+            </footer>
+        </div>
+    );
+}
