c8c700276894ba7acbf8ab134b4bd9dd408cd26d
Artem Kozatskyi <artemfury@gmail.com>
2023-05-25 17:05:56 +0300

    Developed an API for getting products by category

--- Backend/test_project/product/urls.py
+++ Backend/test_project/product/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet, BrandViewSet
+from .views import ProductViewSet, BrandViewSet, ProductsByCategoryListView
 
 app_name = "products"
 
 router = DefaultRouter()
 router.register("brands", BrandViewSet)
 router.register("", ProductViewSet)
+router.register(r'category/(?P<category_slug>[-\w]+)', ProductsByCategoryListView, basename='products-by-category')
 
 urlpatterns = [
     path("", include(router.urls)),
